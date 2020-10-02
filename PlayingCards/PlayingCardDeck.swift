8f007c93bc18ee72a06ea996304cae65832987e0
Helio Hachimine <helio_hachimine_jr@hotmail.com>
2020-10-02 09:44:38 -0300

    first commit

---
+++ PlayingCards/PlayingCardDeck.swift
@@ -0,0 +1,41 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCards
+//
+//  Created by Helio  on 03/04/20.
+//  Copyright © 2020 Helio . All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else{
+            return 0
+        }
+    }
+}
